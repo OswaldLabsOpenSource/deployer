104313b19db3074e82942bd0dfef322ce53a25ff
Anand Chowdhary <mail@anandchowdhary.com>
2019-04-05 22:10:44 +0200

    :sparkles: Add password protection

--- server.ts
+++ server.ts
@@ -10,13 +10,17 @@ server.get("/favicon.ico", (request, reply) => {
   reply.code(200).send();
 });
 
-server.get("/:command", (request, reply) => {
-  deploy(request.params.command)
-    .then(() => reply.code(200).send({ deployer: "success" }))
-    .catch(() => {
-      message("@ceo", `Error in deploying: ${request.params.command}`);
-      reply.code(500).send({ deployer: "error" });
-    });
+server.get("/:command/:password", (request, reply) => {
+  if (request.params.password != <string>process.env.PASSWORD) {
+    reply.code(401).send({ deployer: "error:invalid-password" });
+  } else {
+    deploy(request.params.command)
+      .then(() => reply.code(200).send({ deployer: "success" }))
+      .catch(() => {
+        message("@ceo", `Error in deploying: ${request.params.command}`);
+        reply.code(500).send({ deployer: "error" });
+      });
+  }
 });
 
 server.listen(7004, (error, address) => {
