d53644d55cee79062c2e93ca554631805a9b6019
Anand Chowdhary <mail@anandchowdhary.com>
2019-04-05 21:10:24 +0200

    :sparkles: Add deployment commands

--- index.ts
+++ index.ts
@@ -6,14 +6,29 @@ import { safeLoad } from "js-yaml";
 export const execute = (command: string) =>
   new Promise((resolve, reject) => {
     exec(command, (error, stdout, stderr) => {
-      if (error) return reject(error);
+      if (error || stderr) return reject(error || stderr);
       resolve(stdout);
     });
   });
 
-export const deploy = async (command: string) => {
-  const config = await readFile(join(__dirname, "deployer.yml"));
-  return config;
-}
+export const deploy = async (
+  command:
+    | "agastya-development"
+    | "agastya-staging"
+    | "agastya-production"
+    | "platform"
+    | "berkhan"
+    | "a11yisimportant"
+) => {
+  const config = safeLoad(
+    await readFile(join(__dirname, "..", "deployer.yml"), "utf8")
+  );
+  if (!config[command]) throw new Error(`Invalid command: ${command}`);
+  await execute(config[command].deploy);
+  const message = `Successfully deployed ${config[command].name}`;
+  return message;
+};
 
-deploy("");
+deploy("agastya-development")
+  .then(message => console.log("Done!", message))
+  .catch(error => console.log("got error", error));
