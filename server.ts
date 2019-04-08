be2a4efe4dc8d4fc05f2958fc34f415e7419410d
Anand Chowdhary <mail@anandchowdhary.com>
2019-04-08 11:02:04 +0200

    :zap: Response to request immediately

--- server.ts
+++ server.ts
@@ -14,16 +14,15 @@ server.get("/:command/:password", (request, reply) => {
   if (request.params.password != <string>process.env.PASSWORD) {
     reply.code(401).send({ deployer: "error:invalid-password" });
   } else {
+    reply.code(200).send({ deployer: "deploying" });
     deploy(request.params.command)
-      .then(() => reply.code(200).send({ deployer: "success" }))
+      .then(() => {})
       .catch(() => {
         message("@ceo", `Error in deploying: ${request.params.command}`);
-        reply.code(500).send({ deployer: "error" });
       });
   }
 });
 
 server.listen(7004, (error, address) => {
   if (error) throw new Error(error.message);
-  console.log(`Deployer listening on ${address}`);
 });
